3768ce1bf006108173933b2af90e23aa08d2422e
alvega-g <alvarg@gmail.com>
2023-09-26 16:25:29 +0200

    First push

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 16:06:51 by alvega-g          #+#    #+#             */
+/*   Updated: 2023/09/26 16:09:43 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+
+# endif

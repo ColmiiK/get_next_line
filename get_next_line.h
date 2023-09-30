71c7fc58970c30f16ba7bcd057a2bcd45833b72c
alvega-g <alvarg@gmail.com>
2023-09-30 14:06:10 +0200

    changed for variable buffer

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,21 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 16:06:51 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/30 13:46:15 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/30 14:01:21 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
+// # ifndef BUFFER_SIZE
+// #  define BUFFER_SIZE 10
+// # endif
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 typedef struct s_list
 {
@@ -30,7 +32,7 @@ char				*get_next_line(int fd);
 void				create_list(t_list **list, int fd);
 int					search_newline(t_list *list);
 void				add_to_list(t_list **list, char *buffer);
-char				*get_line(t_list *list);
+char				*create_line(t_list *list);
 
 t_list				*ft_lstlast(t_list *lst);
 int					len_to_newline(t_list *list);

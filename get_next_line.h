83296562ef1b5f6c238748b0a1bf9a1d8fee11b9
alvega-g <alvarg@gmail.com>
2023-10-11 17:45:37 +0200

    correct idndef

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,23 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 15:30:38 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/10/05 15:47:41 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:55:41 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
-unsigned int BUFFER_SIZE = 10;
-
-char *get_next_line(int fd);
-char *create_buffer(char *text, int fd);
-char *copy_up_to_newline(char *text);
+char	*get_next_line(int fd);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);

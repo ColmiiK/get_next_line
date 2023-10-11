483ca2644dc61623051969ffcfe6123d6ed45f73
alvega-g <alvarg@gmail.com>
2023-10-11 17:45:18 +0200

    leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,42 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 15:30:02 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/10/05 17:22:08 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:44:25 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *copy_up_to_newline(char *text)
+static char	*prepare_for_next(char *text)
 {
-	char *result;
-	char *total;
-	int len;
+	int		i;
+	int		j;
+	char	*new_text;
+
+	i = 0;
+	j = 0;
+	while (text[i] && text[i] != '\n')
+		i++;
+	if (!text[i])
+	{
+		free(text);
+		return (0);
+	}
+	new_text = (char *)malloc((ft_strlen(text) - i + 1) * sizeof(char));
+	if (!new_text)
+		return (0);
+	i++;
+	while (text[i])
+		new_text[j++] = text[i++];
+	free(text);
+	return (new_text);
+}
+
+static char	*copy_up_to_newline(char *text)
+{
+	char	*result;
+	char	*total;
+	int		len;
 
 	total = ft_strchr(text, '\n');
 	len = (total - text) + 1;
@@ -24,49 +49,45 @@ char *copy_up_to_newline(char *text)
 	return (result);
 }
 
-char *create_buffer(char *text, int fd)
+static char	*create_buffer(char *text, int fd)
 {
-	char *buffer;
-	int chars_read;
+	char	*buffer;
+	int		chars_read;
 
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (0);
 	buffer[0] = 0;
-	while (!ft_strchr(buffer, '\n'))
+	while (!ft_strchr(buffer, '\n') && chars_read > 0)
 	{
 		chars_read = read(fd, buffer, BUFFER_SIZE);
 		if (chars_read > 0)
 		{
 			buffer[chars_read] = 0;
-			//printf("buffer: %s\ntext: %s\n", buffer, text);
 			text = ft_strjoin(text, buffer);
 		}
 	}
-	free (buffer);
+	free(buffer);
 	return (text);
 }
 
-char *get_next_line(int fd)
-{
-	static char *text;
-	char *result;
+// void	check_leaks(void)
+// {
+// 	system("leaks -q a.out");
+// }
 
+char	*get_next_line(int fd)
+{
+	static char	*text;
+	char		*result;
+
+	if (fd < 0)
+		return (0);
+	// atexit(check_leaks);
 	text = create_buffer(text, fd);
+	if (!text)
+		return (0);
 	result = copy_up_to_newline(text);
-	
+	text = prepare_for_next(text);
 	return (result);
-}
-
-int	main(void)
-{
-	int fd;
-	char *file;
-	char *file2;
-
-	file = "test.txt";
-	fd = open(file, O_RDONLY);
-	printf("%s", get_next_line(fd));
-	
-	close(fd);
 }
